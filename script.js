b499296442edcda8023331fdd8d5e4a1f088b8c4
Akze <57077182+ayy-akshat@users.noreply.github.com>
2021-10-03 22:32:38 +0000

    Added socials in home page and added music links page

---
+++ script.js
@@ -0,0 +1,3 @@
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    alert("This website's layout gets ruined on mobile devices... but you can still navigate in it.");
+}
