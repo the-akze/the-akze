8f1be9fbd47358eb53845bb6cfd4217c78fb61fe
Akze <57077182+ayy-akshat@users.noreply.github.com>
2022-06-02 02:00:12 +0000

    fix nav btns for phone & images on music page not showing, change index.html, add changelog, change og tags

--- script.js
+++
@@ -1,3 +0,0 @@
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    alert("This website's layout gets ruined on mobile devices... but you can still navigate in it.");
-}
