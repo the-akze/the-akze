27a19123dd9eaa5b9e67ceeacd0b0536e2157d37
the-akze <57077182+the-akze@users.noreply.github.com>
2024-01-07 23:53:13 +0000

    ion even know what i changed

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// go to my new domain if they are on the repl one
+// AND if the date and time is before 30 Oct 2023 at 2:00 am PST
+// because of domain expiration if i don't renew it
+if (location.host == 'akze.repl.co' && Date.now() < 1698656400000 && false) { // disabling this now
+    alert("Redirecting from akze.repl.co to www.theakze.xyz! (my new domain)");
+    location = 'https://www.theakze.xyz'
+        + location.pathname
+        + location.search
+        + location.hash;
+}
+
 // DOM Element selections
 const allBlocks = document.getElementsByClassName("block");
 
@@ -7,13 +18,10 @@ const mostY = -20; // 10 or -10
 
 for (var b = 0; b < allBlocks.length; b++) {
     var block = allBlocks[b];
-    console.log(block);
-    console.log(b);
-    console.log(allBlocks);
     block.addEventListener("mousemove", (e) => {
         var elem = e.currentTarget;
         elem.style.transition = '0s';
-        const x = e.pageX - e.currentTarget.offsetLeft; 
+        const x = e.pageX - e.currentTarget.offsetLeft;
         const y = e.pageY - e.currentTarget.offsetTop;
         const { width, height } = elem.getBoundingClientRect();
         const halfWidth = width / 2;
